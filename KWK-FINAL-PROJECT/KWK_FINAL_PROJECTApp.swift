a4aa6e59484e00d661c44b844c5100396a86f4ed
Andrea O <17aolav@gmail.com>
2023-06-27 14:21:53 -0400

    Initial Commit

---
+++ KWK-FINAL-PROJECT/KWK_FINAL_PROJECTApp.swift
@@ -0,0 +1,17 @@
+//
+//  KWK_FINAL_PROJECTApp.swift
+//  KWK-FINAL-PROJECT
+//
+//  Created by Andrea Olavarrieta on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct KWK_FINAL_PROJECTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
